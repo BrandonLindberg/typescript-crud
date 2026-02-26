a7754c9315aef30c2d3e160885b99c00184bdff8
Brandon Lindberg <brandon.s.lindberg@gmail.com>
2026-02-26 16:57:35 -0700

    updated default database

--- src/db.ts
+++ src/db.ts
@@ -4,7 +4,7 @@ import { open } from "sqlite";
 export async function initDB() {
     const db = await open({filename: "./database.sqlite", driver: sqlite3.Database});
 
-    await db.exec(`CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name TEXT NOT NULL)`);
+    await db.exec(`CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name TEXT NOT NULL, quantity INTEGER NOT NULL)`);
 
     return db;
 }
